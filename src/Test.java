5b598da55ecb2f8d58e20a2282c8eaca53bffcfb
zirak1 <zirak786@gmail.com>
2022-01-14 20:11:03 -0500

    test

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,10 @@
 
-public class GitHub {
+public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("X");
-	
-	int x = 10;
-	int y = 5;
-	// 
-	
-	
+// test git
+		
 	}
-	
 
 }
